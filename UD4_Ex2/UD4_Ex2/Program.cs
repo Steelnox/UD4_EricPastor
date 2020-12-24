0620f490ee7cdf01757161a03d55b9ab70994a1e
Steelnox <eric14pastor@gmail.com>
2020-12-24 09:58:32 +0100

    Ejercicio 2

---
+++ UD4_Ex2/UD4_Ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UD4_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string ciudad;
+            Console.WriteLine("Escribe un nombre de persona");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe el nombre de una ciudad");
+            ciudad = Console.ReadLine();
+            Console.WriteLine("Hola {0} bienvenido a {1}", nombre, ciudad);
+        }
+    }
+}
