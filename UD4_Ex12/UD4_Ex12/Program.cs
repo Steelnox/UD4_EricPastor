da90a640f26fcbf6eaf5e98bf88bc91a1bfbf9bc
Steelnox <eric14pastor@gmail.com>
2020-12-24 12:48:29 +0100

    Ejercicio 12

---
+++ UD4_Ex12/UD4_Ex12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UD4_Ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "";
+            Console.WriteLine("Introduce tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Bienvenido " + nombre);
+        }
+    }
+}
