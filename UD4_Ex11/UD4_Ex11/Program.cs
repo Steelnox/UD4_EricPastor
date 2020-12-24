1d7a0ea8cb52dabc2add6708adc96066e481d098
Steelnox <eric14pastor@gmail.com>
2020-12-24 12:43:48 +0100

    Ejercicio 11

---
+++ UD4_Ex11/UD4_Ex11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UD4_Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 27;
+            int num2 = 99;
+
+            if(num1 > num2)
+            {
+                Console.WriteLine("{0} es mayor que {1}", num1, num2);
+            }
+            else if(num2 > num1)
+            {
+                Console.WriteLine("{0} es mayor que {1}", num2, num1);
+            }
+            else 
+            {
+                Console.WriteLine("Los dos numeros son iguales");
+            }
+        }
+    }
+}
