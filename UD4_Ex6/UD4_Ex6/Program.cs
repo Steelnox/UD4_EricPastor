6e4862f5fa51f7d38078373ec15628b76b914c9a
Steelnox <eric14pastor@gmail.com>
2020-12-24 12:22:06 +0100

    Ejercicio 6

---
+++ UD4_Ex6/UD4_Ex6/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace UD4_Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool correcto = false;
+            float precio = 0;
+            string formaPago = "";
+            double cuenta = 0;
+            bool cuentaCorrecta = false;
+
+            while (!correcto)
+            {
+                Console.WriteLine("Introduce un precio");
+                try
+                {
+                    precio = Convert.ToSingle(Console.ReadLine());
+                    if (precio > 0)
+                    {
+                        correcto = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No es un precio correcto. Vuelve a intentarlo");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("No es un precio correcto. Vuelve a intentarlo");
+                }
+            }
+            correcto = false;
+            while (!correcto)
+            {
+                Console.WriteLine("Introduce forma de pago. Efectivo o Tarjeta");
+                formaPago = Console.ReadLine().ToLower();
+                if(formaPago == "efectivo")
+                {
+                    Console.WriteLine("Ha efectuado una compra de {0} euros. Gracias por su compra.", precio);
+                    correcto = true;
+                }
+                else if(formaPago == "tarjeta")
+                {
+                    while (!cuentaCorrecta)
+                    {
+                        Console.WriteLine("Escribe tu numero de cuenta (solo numeros)");
+                        try
+                        {
+                            cuenta = Convert.ToDouble(Console.ReadLine());
+                            cuentaCorrecta = true;
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Numero de cuenta incorrecto");
+                        }
+                    }
+
+                    Console.WriteLine("Ha efectuado una compra de {0} euros a la cuenta {1}. Gracias por su compra.", precio, cuenta);
+                    correcto = true;
+                }
+                else
+                {
+                    Console.WriteLine("Forma de pago incorrecta. Vuelve a intentarlo");
+                }
+            }
+        }
+    }
+}
