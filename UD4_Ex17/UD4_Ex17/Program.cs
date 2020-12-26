27e77107cebe26a4149d124b1601e80041b22a59
Steelnox <eric14pastor@gmail.com>
2020-12-26 20:29:56 +0100

    Ejercicio 17

---
+++ UD4_Ex17/UD4_Ex17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UD4_Ex17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 100)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
