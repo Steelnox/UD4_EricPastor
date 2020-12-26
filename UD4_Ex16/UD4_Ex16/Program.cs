0abe6545eb784e69df456e66ea223e5f06b0f40b
Steelnox <eric14pastor@gmail.com>
2020-12-26 20:27:00 +0100

    Ejercicio 16

---
+++ UD4_Ex16/UD4_Ex16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UD4_Ex16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IVA = 21;
+            float precio = 0;
+            float precioIVA = 0;
+            float precioFinal = 0;
+            bool correcto = false;
+            Console.WriteLine("Introduce un precio por teclado y te devuelvo el precio con IVA incluido");
+            while (!correcto)
+            {
+                try
+                {
+                    precio = Convert.ToSingle(Console.ReadLine());
+                    correcto = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("El numero no es correcto. Vuelve a intentarlo de nuevo");
+                }
+            }
+
+            precioIVA = precio * IVA / 100;
+            precioFinal = precio + precioIVA;
+
+            Console.WriteLine("El precio del producto sin IVA es {0} y con IVA es {1}", precio, precioFinal);
+        }
+    }
+}
