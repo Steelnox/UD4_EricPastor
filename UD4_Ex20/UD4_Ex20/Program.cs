546a0f529edbaed1dd6e98d0b2a4b8812e454421
Steelnox <eric14pastor@gmail.com>
2020-12-26 20:59:57 +0100

    Ejercicio 20

---
+++ UD4_Ex20/UD4_Ex20/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace UD4_Ex20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadVentas = 0;
+            float precioActual = 0;
+            float precioTotal = 0;
+            bool correcto = false;
+            Console.WriteLine("Dime el numero de ventas que has realizado");
+            while (!correcto)
+            {
+                try
+                {
+                    cantidadVentas = Convert.ToInt32(Console.ReadLine());
+                    correcto = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Numero de ventas incorrecto. Vuelve a introducirlo");
+                }
+            }
+            for (int i = 1; i <= cantidadVentas; i++)
+            {
+                correcto = false;
+                Console.WriteLine("Introduce el precio de la venta numero {0}", i);
+                while (!correcto)
+                {
+                    try
+                    {
+                        precioActual = Convert.ToSingle(Console.ReadLine());
+                        correcto = true;                    
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Precio incorrecto. Vuelve a intentarlo");
+                    }
+                }
+                
+                precioTotal += precioActual;
+            }
+
+            Console.WriteLine("El precio total de tus ventas es " + precioTotal);
+        }
+    }
+}
